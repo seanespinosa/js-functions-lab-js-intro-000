da37790e83e155123d0b4b6bd9836045d86830de
Sean Espinosa <sean.espinosa1@gmail.com>
2020-04-07 19:09:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // write your code below!
-function happyHolidays () {
+function happyHolidays() {
   return ("Happy holidays!")
 }
 
-function happyHolidaysTo ()
+function happyHolidaysTo() {
+  
+}
